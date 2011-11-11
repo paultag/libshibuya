4fddf2b557a3b7edfda9a319749fe46a83ea419e
Paul Tagliamonte <paultag@ubuntu.com>
2011-11-10 21:28:19 -0500

    Starting to include the ANSIEscape stuff

---
+++ src/ANSITerminal.cc
@@ -0,0 +1,11 @@
+#include "ANSITerminal.hh"
+
+ANSITerminal::ANSITerminal() {
+}
+ANSITerminal::ANSITerminal( int width, int height ) {
+}
+ANSITerminal::~ANSITerminal() {
+}
+
+void ANSITerminal::insert( unsigned char c ) {
+}
