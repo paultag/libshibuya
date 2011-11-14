paultag/libshibuya

commit f00e75820b23732b18eb5f0345d166bb500a5595

Starting up some ANSI stuff

src/Terminal.cc
@@ -69,13 +69,10 @@ Terminal::Terminal( int width, int height ) {
 }
 
 void Terminal::erase_to_from( int iX, int iY, int tX, int tY ) {
-
 	iY--;
 	tY--;
-
 	int from = GET_OFFSET(iX, iY);
 	int to   = GET_OFFSET(tX, tY);
-
 	for ( int i = from - 1; i < to; ++i ) {
 		this->chars[i].ch   = ' ';
 		this->chars[i].attr = 0x70;

src/ANSITerminal.cc
@@ -21,6 +21,7 @@
  */
 
 #include <ansiescape.hh>
+#include <ansicsi.hh>
 #include <iostream>
 
 #include "ANSITerminal.hh"
@@ -41,8 +42,35 @@ void ANSITerminal::_init_ANSITerminal() {
 }
 
 void ANSITerminal::_handle_escape( ansi_sequence * last ) {
-	/* char mode               = last->mode;
-	std::vector<int> * seqs = last->values; */
+	char mode               = last->mode;
+	std::vector<int> * seqs = last->values;
+	
+	switch ( mode ) {
+		case CSI_ED:
+			/* Clears part of the screen. If n is zero (or missing),
+			 * clear from cursor to end of screen. If n is one,
+			 * clear from cursor to beginning of the screen. If n is two, clear
+			 * entire screen (and moves cursor to upper left on MS-DOS
+			 * ANSI.SYS).*/
+			switch ( seqs->at(0) ) {
+				case -1: /* Missing */
+				case  0:
+					this->erase_to_from( this->cX, this->cY,
+						this->width, this->height );
+					break;
+				case 1:
+					this->erase_to_from( 0, 0, this->cX, this->cY );
+					break;
+				case 2:
+					this->erase_to_from( 0, 0, this->width, this->height );
+					break;
+			}
+			break;
+		default:
+			/* Unknown sequence */
+			break;
+	}
+	
 }
 
 void ANSITerminal::insert( unsigned char c ) {

commit 244f066deaf41bba5453cb65b8b55302c431ea21

Fixing the segfault introduced in f00e75820b23732b18eb5f0345d166bb500a5595
 stupid old code :)

src/Terminal.cc
@@ -69,10 +69,11 @@ Terminal::Terminal( int width, int height ) {
 }
 
 void Terminal::erase_to_from( int iX, int iY, int tX, int tY ) {
-	iY--;
-	tY--;
+	//iY--;
+	//tY--;
 	int from = GET_OFFSET(iX, iY);
 	int to   = GET_OFFSET(tX, tY);
+	std::cerr << from << ", " << to << std::endl;
 	for ( int i = from - 1; i < to; ++i ) {
 		this->chars[i].ch   = ' ';
 		this->chars[i].attr = 0x70;