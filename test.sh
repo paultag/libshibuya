4ace8194a010bd1add20bdd4c927ac95d9660252
Paul Tagliamonte <paultag@ubuntu.com>
2011-11-15 20:38:33 +0000

    annoying pause

--- test.sh
+++ test.sh
@@ -38,12 +38,9 @@ sleep 1
 echo -n "[1J"
 sleep 1
 
-
 clear
 echo -n "[H"
-
 echo -n "This is a rather long line. Im using it to test removing bits of a str"
-sleep 1
 echo -n "[40G" # Center the cursor on the current line
 sleep 1
 echo -n "[K"
