37e605af6dd42af36728754976288b8ef34fd995
Paul Tagliamonte <paultag@ubuntu.com>
2011-11-16 16:03:13 +0000

    small cosmetic change

--- tests/run.sh
+++ tests/run.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+REVID=`git log | grep commit | head -n 1 | awk '{print $2}'`
+
 echo "Running tests. Please ensure you have a curses-aware terminal."
 echo ""
+echo " == DBG info =="
+echo "  Revno: $REVID"
+echo ""
+echo "Hit enter to run tests."
+echo ""
+
+read foo
+
 ./bin/run-test
 if [ $? -ne 0 ]; then
 	echo ""
